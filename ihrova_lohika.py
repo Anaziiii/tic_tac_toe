a56b1c8dcad174805e5012ed04ce7aeb493a1aea
HeraRoman <detrek1234@gmail.com>
2025-06-17 21:06:47 +0300

    drygyy kod

--- ihrova_lohika.py
+++ ihrova_lohika.py
@@ -1,10 +1,11 @@
-def check_winer(board):
-    for row in board:
-        if row[0] == row[1] ==  row[2]:
-            return row[0]
-    for col  in range(3):
-        if board[0][col] == board[1][col] == board[2][col]:
+def check_winer():
+    for row in range(BOARD_SIZE):
+        if board[row][0] == board[row][1] ==  board[row][2] and board[row][0]:
+            return board[row][0]
+    for col  in range(BOARD_SIZE):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col]:
             return board[0][col]
-    if board[0][0] == board[1][1] == board[2][2]:
-        return board[1][1]
-    return None
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2]:
+        return board[0][2]
