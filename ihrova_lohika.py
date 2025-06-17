b47265b87bcb11ddbdbef0cdc6575476665a405a
HeraRoman <detrek1234@gmail.com>
2025-06-17 21:44:33 +0300

    Create ihrova_lohika.py

---
+++ ihrova_lohika.py
@@ -0,0 +1,11 @@
+def check_winer():
+    for row in range(BOARD_SIZE):
+        if board[row][0] == board[row][1] ==  board[row][2] and board[row][0]:
+            return board[row][0]
+    for col  in range(BOARD_SIZE):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col]:
+            return board[0][col]
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2]:
+        return board[0][2]
